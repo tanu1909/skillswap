7dd06bbaf60ba16e1bc3db4ff32793b072a93a98
Tanu Choudhary <tanuchoudhary2403@gmail.com>
2026-06-24 10:18:55 +0530

    .

--- node_modules/skillspace-backend/app.js
+++ node_modules/skillspace-backend/app.js
@@ -14,7 +14,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:true,
+    credentials: true
+}
+));
 app.use(express.json());
 
 // Mount API Routes
